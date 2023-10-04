fed5289790dad0327f4ca21942d8a9144b3f0059
Gabriel Pavanin <gabrielps.map@gmail.com>
2023-10-04 13:36:55 -0300

    componente de pesquisa

---
+++ src/components/Search/dadosPesquisa.js
@@ -0,0 +1,8 @@
+import livro from "../../img/livro.png";
+
+export const livros = [
+  { nome: "Liderança em Desigin", id: 1, src: livro },
+  { nome: "Casa do Código", id: 2, src: livro },
+  { nome: "Python é o Máximo", id: 3, src: livro },
+  { nome: "Não gosto de Java", id: 4, src: livro },
+];
