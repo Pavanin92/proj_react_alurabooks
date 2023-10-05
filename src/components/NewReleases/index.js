0b2004260272c1db0033e8de06fcf06729afdf56
Gabriel Pavanin <gabrielps.map@gmail.com>
2023-10-05 11:26:06 -0300

    finalizando projeto

--- src/components/NewReleases/index.js
+++ src/components/NewReleases/index.js
@@ -1,4 +1,7 @@
 import { livros } from "./newReleaseData";
+import { Titulo } from "../Title";
+import CardRecomenda from "../CardRecomenda";
+import imgLivro from "../../img/livro2.png";
 import styled from "styled-components";
 
 const ReleaseContainer = styled.section`
@@ -6,16 +9,6 @@ const ReleaseContainer = styled.section`
   padding-bottom: 20px;
   display: flex;
   flex-direction: column;
-`;
-
-const Titulo = styled.h2`
-  width: 100%;
-  padding: 30px 0;
-  background-color: #fff;
-  color: #eb9b00;
-  font-size: 36px;
-  text-align: center;
-  margin: 0;
 `;
 
 const NewBooksContainer = styled.div`
@@ -29,12 +22,20 @@ const NewBooksContainer = styled.div`
 function NewReleases() {
   return (
     <ReleaseContainer>
-      <Titulo>Últimos Lançamentos</Titulo>
+      <Titulo cor="#EB9B00" fontSize="36px">
+        Últimos Lançamentos
+      </Titulo>
       <NewBooksContainer>
-        { livros.map((livro) => {
+        {livros.map((livro) => {
           return <img src={livro.src} alt="capa do livro" />;
         })}
       </NewBooksContainer>
+      <CardRecomenda 
+        title="Talvez você se interesse por..."
+        subtitle="Angular 11"
+        description="Construindo uma aplicação integrada com a plataforma Google."
+        img={imgLivro}
+      />
     </ReleaseContainer>
   );
 }
